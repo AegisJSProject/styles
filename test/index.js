52f382dc311d4314f11666d8e42abe474d724a92
Chris Zuber <shgysk8zer0@gmail.com>
2024-09-19 10:47:38 -0700

    Update dependencies and config

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ import { addStyles } from '@aegisjsproject/core';
 
 addStyles(document, btn, reset, baseTheme, darkTheme, lightTheme);
 
-document.getElementById('toggle').addEventListener('click', () => {
+document.getElementById('toggle').addEventListener('click', async () => {
 	switch(document.documentElement.dataset.theme) {
 		case 'light':
 			document.documentElement.dataset.theme = 'dark';
@@ -18,4 +18,19 @@ document.getElementById('toggle').addEventListener('click', () => {
 	}
 
 	document.getElementById('cur').textContent = document.documentElement.dataset.theme;
+
+	await cookieStore.set({
+		name: 'theme',
+		value: document.documentElement.dataset.theme,
+		path: '/test/',
+		sameSite: 'strict',
+		secure: true,
+	});
+});
+
+cookieStore.get('theme').then(cookie => {
+	if (typeof cookie?.value === 'string') {
+		document.documentElement.dataset.theme = cookie.value;
+		document.getElementById('cur').textContent = document.documentElement.dataset.theme;
+	}
 });
