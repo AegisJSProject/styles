e278fcef572dd289ed2ba20629c52d7b7f3e2387
Chris Zuber <shgysk8zer0@gmail.com>
2024-09-26 07:09:03 -0700

    Add custom properties and fix `display`
    ### Added
    - Begin adding custom properties
    - Add `@properties` via `properties.js`
    - Add theme inheritance via `:host-context()` (where supported)
    
    ### Fixed
    - Do not set `display` for `hidden` or `popover` elements

--- theme.js
+++ theme.js
@@ -3,59 +3,80 @@ import { css, darkCSS, lightCSS } from '@aegisjsproject/core/parsers/css.js';
 
 export const baseTheme = css`:root {
 	color-scheme: light dark;
-	color: ${dark};
-	background-color: ${light};
+	color: var(--aegis-color-light, ${dark});
+	background-color: var(--aegis-bg-light, ${light});
 	font-family: system-ui;
 }
 
 :root[data-theme="light"] {
 	color-scheme: light;
-	color: ${dark};
-	background-color: ${light};
+	color: var(--aegis-color-light, ${dark});
+	background-color: var(--aegis-bg-light, ${light});
 }
 
 :root[data-theme="dark"] {
 	color-scheme: dark;
-	color: ${light};
-	background-color: ${dark};
+	color: var(--aegis-color-dark, ${light});
+	background-color: var(--aegis-bg-dark, ${dark});
 }`;
 
 export const darkTheme = darkCSS`:root:not([data-theme="light"]) {
-	color: ${light};
-	background-color: ${dark};
+	color: var(--aegis-color-dark, ${light});
+	background-color: var(--aegis-bg-dark, ${dark});
 }`;
 
 export const lightTheme = lightCSS`:root:not([data-theme="dark"]) {
-	color: ${dark};
-	background-color: ${light};
+	color: var(--aegis-color-light, ${dark});
+	background-color: var(--aegis-bg-light, ${light});
 }`;
 
 export const componentBase = css`:host {
-	display: block;
 	color-scheme: light dark;
-	color: ${dark};
-	background-color: ${light};
+	color: var(--aegis-color-light, ${dark});
+	background-color: var(--aegis-bg-light, ${light});
 	font-family: system-ui;
+}
+
+/*:host(:not([popover])) {
+	display: block;
+}*/
+
+:host(:not([hidden]):not([popover])) {
+	display: block;
+}
+
+:host-context([data-theme="light"]):host(:not([theme="dark"])) {
+	color-scheme: light;
+	color: var(--aegis-color-light, ${dark});
+	background-color: var(--aegis-bg-light, ${light});
+}
+
+:host-context([data-theme="dark"]):host(:not([theme="light"])) {
+	color-scheme: dark;
+	color: var(--aegis-color-dark, ${light});
+	background-color: var(--aegis-bg-dark, ${dark});
 }
 
 :host([theme="light"]) {
 	color-scheme: light;
+	color: var(--aegis-color-light, ${dark});
+	background-color: var(--aegis-bg-light, ${light});
 }
 
 :host([theme="dark"]) {
 	color-scheme: dark;
-	color: ${light};
-	background-color: ${dark};
+	color: var(--aegis-color-dark, ${light});
+	background-color: var(--aegis-bg-dark, ${dark});
 }`;
 
 export const componentDarkTheme = darkCSS`:host(:not([theme="light"])) {
 	color-scheme: dark;
-	color: ${light};
-	background-color: ${dark};
+	color: var(--aegis-color-dark, ${light});
+	background-color: var(--aegis-bg-dark, ${dark});
 }`;
 
 export const componentLightTheme = lightCSS`:host(:not([theme="dark"])) {
 	color-scheme: light;
-	color: ${dark};
-	background-color: ${light};
+	color: var(--aegis-color-light, ${light});
+	background-color: var(--aegis-bg-light, ${dark});
 }`;
